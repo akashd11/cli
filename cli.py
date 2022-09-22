117717dbda1e754ce9584316fb0c2137af9e9923
Akash Deep <akashdeep@D11-Akash.Deep>
2022-09-22 15:12:44 +0530

    cli

---
+++ cli.py
@@ -0,0 +1,22 @@
+import typer
+import comp
+
+app = typer.Typer()
+
+
+@app.command()
+def create(name: str):
+    comp.create(name)
+
+
+@app.command()
+def delete(name: str, formal: bool = False):
+    comp.delete(name, formal)
+
+
+def cli():
+    app()
+
+
+
+
